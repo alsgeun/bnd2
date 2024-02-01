05260e7bc653e82c0601d4335d8afed8a91c55dd
unknown <mgeun97@naver.com>
2024-02-01 17:40:03 +0900

    prisma model 추가

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-import express from 'express';
+import express from "express";
+
+const app = express();
+const PORT = 2024;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(PORT, "포트로 서버가 열렸어요!");
+});
