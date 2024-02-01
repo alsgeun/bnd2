7a6cb50282ca08b41b22699589c1f025a2a2ba6c
unknown <mgeun97@naver.com>
2024-02-01 23:48:28 +0900

    회원가입시 비밀번호 재확인 기능, 비밀번호 6글자 이상 설정 기능 추가

--- router/users.router.js
+++ router/users.router.js
@@ -1,23 +1,35 @@
 import express from "express";
 import { prisma } from "../src/utils/prisma/index.js";  // 프리즈마 클라이언트 소환
+import bcrypt from "bcrypt";    // 비크립트 소환!
 
 const router = express.Router();    // 라우터 소환
 
 // 회원가입 api
 router.post('/sign-up', async(req, res, next) => {
-    const { email, password, name, age, gender,character, profileImage } = req.body;  // 회원가입시 이런걸 적어라 하고 body에 담아 서버에게 요청
+    const { email, password, confirmPassword, name, age, gender,character, profileImage, } = req.body;  // 회원가입시 이런걸 적어라 하고 body에 담아 서버에게 요청
     
     // 동일한 e메일 사용자 있는지 확인
     const isExistUser = await prisma.users.findFirst({  // 프리즈마 클라이언트로 users 테이블에서 찾아내고 변수에 저장
         where : { email }
     })
+    
+    if (confirmPassword !== password) {
+        return res.status(409).json({ message : '비밀번호가 일치 하지 않습니다.'});
+    }
     if (isExistUser) {  // 동일한 이메일로 가입한 사용자가 있을 경우
         return res.status(409).json({ message : '이미 존재하는 이메일 입니다.'});
         }
-
+    if (password.length < 6) {  // 비밀번호 6글자 이하시 에러 메세지 전송
+        return res.status(409).json({ message : '비밀번호를 6글자 이상 입력 해주세요.'});
+    }
+    // 비밀번호 암호화
+    const hashedPassword = await bcrypt.hash (password, 10); // 비크립트를 통해 다져서 못알아보게 한다.(.hash) 뭐를? 비밀번호를(password,) 몇번? 10번.(10)
     // 사용자 생성
     const user = await prisma.users.create({    // users 모델에서 생성
-        data : { email, password }              // email과 password를 이용해서 생성
+        data : { 
+            email,
+            password : hashedPassword,
+         }              // email과 password를 이용해서 생성, 비밀번호는 hashedPassword(암호화된 비밀번호 형태)로 저장
     })
     // 사용자 정보 생성
     const userInfo = await prisma.userInfos.create({
